073b3caec8888bbbb2e60ea679ec58df65bd7603
Miao <xyz00029299@gmail.com>
2021-02-08 08:33:36 +1000

    Update toscrape-css.py
    spider for recipes

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -7,15 +7,16 @@ class ToScrapeCSSSpider(scrapy.Spider):
     start_urls = [
        # 'http://quotes.toscrape.com/tag/love/',
        # 'https://www.flowersforeveryone.com.au/',
-        'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=11',
+       # 'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=11',
+        'https://soyummy.com/breakfast/',
     ]
 
     def parse(self, response):
-        for quote in response.css("div.rs_cnt"):
+        for quote in response.css("div.post-preview-box"):
             yield {
-                'text': quote.css("h3 a::text").extract_first(),
-    #            'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.fl_w100 > span::text").extract()
+                'text': quote.css("a::attr(href)").extract_first(),
+                'author': quote.css("div.post-preview-box-title::text").extract_first(),
+     #           'tags': quote.css("div.fl_w100 > span::text").extract()
             }
 
  #       next_page_url = response.css("li.next > a::attr(href)").extract_first()
