40ec46a9f71ec2964758e469b177946b98f777a4
Miao <xyz00029299@gmail.com>
2021-02-07 15:32:32 +1000

    Update toscrape-css.py
    plants

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -5,15 +5,16 @@ import scrapy
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
     start_urls = [
-        'http://quotes.toscrape.com/tag/love/',
+       # 'http://quotes.toscrape.com/tag/love/',
+        'https://www.flowerpower.com.au/products/plants',
     ]
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        for quote in response.css("div.product-item"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
+                'text': quote.css("strong.product-item-name::text").extract_first(),
+    #            'author': quote.css("small.author::text").extract_first(),
+    #            'tags': quote.css("div.tags > a.tag::text").extract()
             }
 
  #       next_page_url = response.css("li.next > a::attr(href)").extract_first()
