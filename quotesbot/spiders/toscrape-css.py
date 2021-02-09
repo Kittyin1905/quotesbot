40e5765494e8296d4c270577785138137a556643
Miao <xyz00029299@gmail.com>
2021-02-09 22:03:19 +1000

    Update toscrape-css.py
    together

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
-class Product(scrapy.Item):
+class Ingredient(scrapy.Item):
     name = scrapy.Field()
     price = scrapy.Field()
     stock = scrapy.Field()
-class step(scrapy.Item):
+class Step(scrapy.Item):
     name = scrapy.Field()
-    content = scrapy.Field()
+ 
    
     
 class ToScrapeCSSSpider(scrapy.Spider):
@@ -21,20 +21,28 @@ class ToScrapeCSSSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-      
+        step=Step(),
+        ingredient=Step(),
         self.logger.info('A mmparse response from %s just arrived!', response.url),
-        for quote in response.css("li.single-recipe-page-step"):
-            yield {      
-                'author': quote.css("div.single-recipe-page-step__title::text").extract_first(),
-            }
-        for quote in response.css("li.single-recipe-page__ingredient"):
-            yield {
+        step[name]=response.css("li.single-recipe-page-step::text").extract(),
+        ingredient[name]=response.css("li.single-recipe-page__ingredient::text").extract(),
+ #       for quote in response.css("li.single-recipe-page-step"):
+   #         step[name]=quote.css("div.single-recipe-page-step__title::text").extract_first()
+   #         yield {      
+ #               'author': quote.css("div.single-recipe-page-step__title::text").extract_first(),
+  #          }
+  #      for quote in response.css("li.single-recipe-page__ingredient"):
+  #          ingredient= quote.css("li.single-recipe-page__ingredient::text").extract_first()
+       #     yield{
          #       'text': quote.css("div.single-recipe-page-info-item_content > div::text").extract(),
-                'ingredient': quote.css("li.single-recipe-page__ingredient::text").extract_first(),
+     #           'ingredient': quote.css("li.single-recipe-page__ingredient::text").extract_first(),
        #         'text': quote.css("a::attr(href)").extract_first(),
        #         'author': quote.css("div.post-preview-box-title::text").extract_first(),
      #           'tags': quote.css("div.fl_w100 > span::text").extract()
-            }
+     #       }
+         yield{step,
+               ingredient
+         }
  #       next_page_url = response.css("li.next > a::attr(href)").extract_first()
  #       if next_page_url is not None:
  #           yield scrapy.Request(response.urljoin(next_page_url))
