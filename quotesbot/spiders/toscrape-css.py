8c28c01da8cfe3abd9a00daca53b5656bdfc402a
Miao <xyz00029299@gmail.com>
2021-02-09 14:10:50 +1000

    Update toscrape-css.py
    content

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -8,14 +8,17 @@ class ToScrapeCSSSpider(scrapy.Spider):
        # 'http://quotes.toscrape.com/tag/love/',
        # 'https://www.flowersforeveryone.com.au/',
        # 'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=11',
-        'https://soyummy.com/breakfast/',
+       # 'https://soyummy.com/breakfast/',
+        'https://soyummy.com/recipe/triple-chocolate-puffs/',
     ]
 
     def parse(self, response):
-        for quote in response.css("div.post-preview-box"):
+        for quote in response.css("div.single-recipe-page-info-item_content"):
             yield {
-                'text': quote.css("a::attr(href)").extract_first(),
-                'author': quote.css("div.post-preview-box-title::text").extract_first(),
+                'text': quote.css("div.single-recipe-page-info-item_title::text").extract_first(),
+                'author': quote.css("div.single-recipe-page-info-item_text::text").extract_first(),
+       #         'text': quote.css("a::attr(href)").extract_first(),
+       #         'author': quote.css("div.post-preview-box-title::text").extract_first(),
      #           'tags': quote.css("div.fl_w100 > span::text").extract()
             }
 
