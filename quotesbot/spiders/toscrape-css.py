9db10216c24daf1911e27cd5296f23c3b3783ef8
Miao <xyz00029299@gmail.com>
2021-02-10 14:28:18 +1000

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -17,8 +17,8 @@ class ToScrapeCSSSpider(scrapy.Spider):
         yield {
             'title': content.css("h1.content__title::text").extract(),
             'ingredients': text.css("li.single-recipe-page__ingredient::text").extract(),
-            'description': content.css("div.description-container::text").extract(),
-            'subtitle': content.css("div.content__category::text").extract(),
+ #           'description': content.css("div.description-container::text").extract(),
+#            'subtitle': content.css("div.content__category::text").extract(),
 #            'complexity': comp.css("div.single-recipe-page-info-item__text::text").extract(),
 #            'cooking_time': content.css("div.single-recipe-page-info-item__text::text").extract()[1],
 #             'prep_time': content.css("div.single-recipe-page-info-item__text::text").extract()[0],
