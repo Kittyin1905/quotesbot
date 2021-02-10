db2cdaf156c70428fad1f02b35eb7fa9df928711
Miao <xyz00029299@gmail.com>
2021-02-10 12:39:55 +1000

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -15,9 +15,19 @@ class ToScrapeCSSSpider(scrapy.Spider):
         content= response.css("div.content-container")[0]
         text= response.css("ul.single-recipe-page-ingredients__list")[0]
         yield {
+            'complexity': content.css("div.single-recipe-page-info-item__text::text").extract()[2],
+            'cooking_time': content.css("div.single-recipe-page-info-item__text::text").extract()[1],
+            'prep_time': content.css("div.single-recipe-page-info-item__text::text").extract()[0],
+            'serves': content.css("div.single-recipe-page-info-item__text::text").extract()[3],
+            'description': content.css("div.description-container::text").extract(),
+            'subtitle': content.css("div.content__category::text").extract(),
             'title': content.css("h1.content__title::text").extract(),
             'ingredients': text.css("li.single-recipe-page__ingredient::text").extract(),
-            'steps': content.css("div.single-recipe-page-step__title::text").extract()
+#             for element in content.css("li.single-recipe-page-step"):
+#                 'step[title]': content.css("div.single-recipe-page-step__title::text").extract()
+#                 'step[text]': content.css("div.single-recipe-page-step__text span::text").extract()
+#             'steps':"title:" + content.css("div.single-recipe-page-step__title::text").extract()+
+#             "text:" + content.css("div.single-recipe-page-step__text::text").extract()+
         }
         
 # class Step(scrapy.Item):
