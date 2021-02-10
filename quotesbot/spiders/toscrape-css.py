47fee414d4c33ecdf4b13a0f6893856219b2f884
Miao <xyz00029299@gmail.com>
2021-02-10 23:31:27 +1000

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -1,15 +1,60 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class MySpider(CrawlSpider):
+    name = 'soyummy.com'
+    allowed_domains = ['soyummy.com']
+    start_urls = ['https://soyummy.com/breakfast/']
+
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        Rule(LinkExtractor(allow=('page', ), deny=('subsection\.php', ))),
+
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=('recipe', )), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        content= response.css("div.content-container")[0]
+        text= response.css("ul.single-recipe-page-ingredients__list")[0]
+        temp= response.css("div.single-recipe-page-info-item")
+
+        yield {
+            'title': content.css("h1.content__title::text").extract_first(),
+            'subtitle': content.css("div.content__category::text").extract_first(),
+            'description': content.css("div.description-container::text").extract_first(),
+            'serves': temp[3].css("div.single-recipe-page-info-item__text::text").extract_first(),
+            'prep_time': temp[0].css("div.single-recipe-page-info-item__text::text").extract_first(),
+            'cooking_time': temp[1].css("div.single-recipe-page-info-item__text::text").extract_first(),
+            'complexity': temp[2].css("div.single-recipe-page-info-item__text::text").extract_first(),
+            'ingredients': text.css("li.single-recipe-page__ingredient::text").extract(),
+            'steps_title': content.css("div.single-recipe-page-step__title::text").extract(),
+            'steps_text': content.css("div.single-recipe-page-step__text span::text").extract()
+        }
+
+        item = scrapy.Item()
+        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['name'] = response.xpath('//td[@id="item_name"]/text()').get()
+        item['description'] = response.xpath('//td[@id="item_description"]/text()').get()
+        item['link_text'] = response.meta['link_text']
+        url = response.xpath('//td[@id="additional_data"]/@href').get()
+        return response.follow(url, self.parse_additional_page, cb_kwargs=dict(item=item))
+
+    def parse_additional_page(self, response, item):
+        item['additional_data'] = response.xpath('//p[@id="additional_data"]/text()').get()
+        return item
 
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
     start_urls = [
-       # 'http://quotes.toscrape.com/tag/love/',
-       # 'https://www.flowersforeveryone.com.au/',
         'https://soyummy.com/recipe/glazed-lemon-poppy-seed-pastries/',
-      #  'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=11',
     ]
+    
     def parse(self, response):
         content= response.css("div.content-container")[0]
         text= response.css("ul.single-recipe-page-ingredients__list")[0]
@@ -28,28 +73,8 @@ class ToScrapeCSSSpider(scrapy.Spider):
             'steps_text': content.css("div.single-recipe-page-step__text span::text").extract()
         }
 
-#             for element in content.css("li.single-recipe-page-step"):
-#                 'step[title]': content.css("div.single-recipe-page-step__title::text").extract()
-#                 'step[text]': content.css("div.single-recipe-page-step__text span::text").extract()
-#             'steps':"title:" + content.css("div.single-recipe-page-step__title::text").extract()+
-#             "text:" + content.css("div.single-recipe-page-step__text::text").extract()+
+ 
         
-        
-# class Step(scrapy.Item):
-#     name = scrapy.Field()
-     
-# class ToScrapeCSSSpider(scrapy.Spider):
-#     name = "toscrape-css"
-#     start_urls = [
-#         'https://soyummy.com/recipe/glazed-lemon-poppy-seed-pastries/',
-#     ]
-  
-        
-#     def parse(self, response):
-#         self.logger.info('A parse1 response from %s just arrived!', response.url)
-#         text= response.css("ul.single-recipe-page-ingredients__list")[0]
-#         yield {
-#             'ingredients': text.css("li.single-recipe-page__ingredient::text").extract()
-#         }
+
         
         
