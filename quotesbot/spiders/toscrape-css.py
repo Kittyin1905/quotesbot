f8e908ac3c18381f9ce82df2aca8f618a2b8904d
Miao <xyz00029299@gmail.com>
2021-02-10 14:31:17 +1000

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -13,7 +13,7 @@ class ToScrapeCSSSpider(scrapy.Spider):
     def parse(self, response):
         content= response.css("div.content-container")[0]
         text= response.css("ul.single-recipe-page-ingredients__list")[0]
-        comp = response.css("div.single-recipe-page-info-item")[0]
+
         yield {
             'title': content.css("h1.content__title::text").extract(),
             'ingredients': text.css("li.single-recipe-page__ingredient::text").extract(),
@@ -24,8 +24,8 @@ class ToScrapeCSSSpider(scrapy.Spider):
 #             'prep_time': content.css("div.single-recipe-page-info-item__text::text").extract()[0],
 #             'serves': content.css("div.single-recipe-page-info-item__text::text").extract()[3],
              
-            'title': content.css("h1.content__title::text").extract(),
-            'ingredients': text.css("li.single-recipe-page__ingredient::text").extract()
+#             'title': content.css("h1.content__title::text").extract(),
+#             'ingredients': text.css("li.single-recipe-page__ingredient::text").extract()
         
         }
             
