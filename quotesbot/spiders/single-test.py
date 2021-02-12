c0360e5c35e0ab185dbbcd0d61d84fa042ebe6ef
Miao <xyz00029299@gmail.com>
2021-02-12 17:24:34 +1000

    Update single-test.py

--- quotesbot/spiders/single-test.py
+++ quotesbot/spiders/single-test.py
@@ -5,16 +5,15 @@ import scrapy
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "item-test"
     start_urls = [
-        'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=9',
-   #     'https://cricos.education.gov.au/Course/CourseDetails.aspx?CourseId=103601',
+   #     'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=9',
+        'https://cricos.education.gov.au/Course/CourseDetails.aspx?CourseId=103601',
     ]
 
-    def parse(self, response):
-        content= response.xpath('//div[@class="brd_btm"]')[0]
-    #    content= response.css("div.brd_btm")[0]
+    def parse(self, response)
+        content= response.css("div.container")[0]
       #  fee= response.xpath('//div[@id="ctl00_cphDefaultPage_courseDetail_trTuition"]')[0]
         yield {
-            'title': content.css("h2.univ_tit a::text").extract_first()
+            'title': content.css("h1::text").extract_first()
      #       'title': content.xpath('//h2[@class="univ_tit"]/text()').extract_first(),
      #       'tuition_fee': fee.xpath('//span[@id="ctl00_cphDefaultPage_courseDetail_lblTuition"]/text()').extract_first(),
             
