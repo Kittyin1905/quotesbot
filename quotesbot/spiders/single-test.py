2c17a772293fab617b64732e6f8cec0f06420722
Miao <xyz00029299@gmail.com>
2021-02-12 17:26:38 +1000

    Update single-test.py

--- quotesbot/spiders/single-test.py
+++ quotesbot/spiders/single-test.py
@@ -14,10 +14,11 @@ class ToScrapeCSSSpider(scrapy.Spider):
       #  fee= response.xpath('//div[@id="ctl00_cphDefaultPage_courseDetail_trTuition"]')[0]
         yield {
             'title': content.css("h1::text").extract_first()
+        }
      #       'title': content.xpath('//h2[@class="univ_tit"]/text()').extract_first(),
      #       'tuition_fee': fee.xpath('//span[@id="ctl00_cphDefaultPage_courseDetail_lblTuition"]/text()').extract_first(),
             
-        }
+        
 
 #         next_page_url = response.css("li.next > a::attr(href)").extract_first()
 #         if next_page_url is not None:
