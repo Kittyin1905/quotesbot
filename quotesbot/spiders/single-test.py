1d12855d4860a032222d7a4080fadc8e3251f003
Miao <xyz00029299@gmail.com>
2021-02-12 17:10:51 +1000

    Update single-test.py

--- quotesbot/spiders/single-test.py
+++ quotesbot/spiders/single-test.py
@@ -5,14 +5,16 @@ import scrapy
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "item-test"
     start_urls = [
-        'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=9',
+   #     'https://search.studyinaustralia.gov.au/course/search-results.html?qualificationid=9',
+        'https://cricos.education.gov.au/Course/CourseDetails.aspx?CourseId=103601',
     ]
 
     def parse(self, response):
-        content= response.css("div.brd_btm")[0]
+        content= response.xpath('//div[@class="brd_btm"]')
+     #   content= response.css("div.brd_btm")[0]
       #  fee= response.xpath('//div[@id="ctl00_cphDefaultPage_courseDetail_trTuition"]')[0]
         yield {
-            'title': content.css("h2.univ_tit::text").extract_first()
+            'title': content.css("h2.univ_tit a::text").extract_first()
      #       'title': content.xpath('//h2[@class="univ_tit"]/text()').extract_first(),
      #       'tuition_fee': fee.xpath('//span[@id="ctl00_cphDefaultPage_courseDetail_lblTuition"]/text()').extract_first(),
             
